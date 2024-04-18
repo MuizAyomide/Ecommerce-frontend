64edfc7b40961d7f2eb0592172ba0eb75612e124
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-18 13:12:57 +0100

    cartitem creation

---
+++ src/Components/CartItems/CartItem.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import "./CartItems.css";
+import { ShopContext } from "../../Contexts/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
+import Cart from "../../Pages/Cart";
+
+const CartItem = () => {
+  const { getTotalCartAmount, all_product, cartItem, removeFromCart } = useContext(ShopContext);
+
+  return (
+    <div className="cartItems">
+      <div className="cartItems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItem[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartItems-format cartItems-format-main">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartItems-quantity">{cartItem[e.id]}</button>
+                <p>${e.new_price * cartItem[e.id]}</p>
+                <img
+                  className="cartItem-remove-icon"
+                  src={remove_icon}
+                  alt=""
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+
+return null;
+
+      })}
+      <div className="cartItems-down">
+        <div className="cartItems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartItems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cartItems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartItems-total-item">
+              <h3>Total</h3>
+              <h3>${getTotalCartAmount()}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartItems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartItems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
