2e7298b07f51cdd38f73b483800fe6c6efbe86fd
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-26 08:50:57 +0100

    commit

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -6,8 +6,8 @@ import './NewsLetter.css'
 
 const NewsLetter = () => {
   return (
-    <div className='newsletter'>
-      <h1>Get Eclusive Offers On Your Email</h1>
+    <div className='newsletter container'>
+      <h1>Get Exclusive Offers On Your Email</h1>
       <p>Subcribe to our newsletter and stay updated</p>
       <div>
         <input type="email" placeholder='Your Email id' />
