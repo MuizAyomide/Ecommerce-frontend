e1dbcb3d030aac47285da23c99bf484450df7c96
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-26 08:49:14 +0100

    commit

--- src/Components/DiscriptionBox.jsx/DescriptionBox.jsx
+++ src/Components/DiscriptionBox.jsx/DescriptionBox.jsx
@@ -3,7 +3,7 @@ import './DiscriptionBox.css'
 
 const DescriptionBox = () => {
   return (
-    <div className='descriptionbox'>
+    <div className='descriptionbox container'>
       <div className="descriptionbox-navigator">
         <div className="descriptionbox-nav-box">Description</div>
         <div className="descriptionbox-nav-box fade">Reviews (122)</div>
