f45c1118e5c13167fb791ca1ef7b444a99022a50
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-26 08:50:22 +0100

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,13 +6,14 @@ import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Contexts/ShopContext'
 
 
+
 const Navbar = () => {
 
   const [menu,setMenu] = useState('')
   const {getTotalCartItems} = useContext(ShopContext);
 
   return (
-    <div className='navbar'>
+    <div className='navbar container'>
         <div className='nav-logo'>
             <img src={logo} alt="" />
             <p>SHOPIFY</p>
@@ -22,11 +23,10 @@ const Navbar = () => {
             <li onClick={()=> setMenu('mens')}><Link to='/mens' style={{textDecoration: 'none'}}>  Men</Link>{menu ==='mens'?<hr />:''}</li>
             <li onClick={()=> setMenu('womens')}><Link to='/womens' style={{textDecoration: 'none'}}>   Women</Link>{menu ==='womens'?<hr />:''}</li>
             <li onClick={()=> setMenu('kids')}> <Link to='/kids' style={{textDecoration: 'none'}}>Kids </Link>{menu ==='kids'?<hr />:''}</li>
-
         </ul>
         <div className="nav-login-cart">
       <Link to='/login'><button>Login</button></Link>      
-         <Link to='./cart'><img src={cart_icon} alt="" /></Link>   
+         <Link to='./cart'><img src={cart_icon} alt="" className='cart' /></Link>   
             <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
