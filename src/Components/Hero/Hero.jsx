a9124025996e3a1a5e047011b1040182bc272809
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-22 00:03:53 +0100

    commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ import hero_image from '../Assets/hero_image.png'
 
 
 
+
 const Hero = () => {
   return (
     <div className='hero container'>
@@ -17,6 +18,7 @@ const Hero = () => {
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
             <div className="hero-hand-icon">
+              
                 <p>new</p>
                 <img src={hand_icon} alt="" />
             </div>
