4935d9aa2c319220338ea02b17b5ad77518c427c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-22 00:04:33 +0100

    commit

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -3,10 +3,12 @@ import './Offers.css'
 import exclusive_img from '../Assets/exclusive_image.png'
 
 
+
 const Offers = () => {
   return (
     <div className='offers container'>
       <div className='offers-left'>
+        
             <h1>Exclusive</h1>
             <h1>Offers For You</h1>
             <p>ONLY ON BEST SELLERS PRODUCTS</p>
